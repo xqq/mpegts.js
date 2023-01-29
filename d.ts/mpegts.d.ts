5f9e9d01f657f191ee3598b8165fb58b051e8acb
tsukumi <tsukumijima@users.noreply.github.com>
2023-01-30 04:32:58 +0900

    d.ts: Update definitions

--- d.ts/mpegts.d.ts
+++ d.ts/mpegts.d.ts
@@ -345,6 +345,8 @@ declare namespace Mpegts {
     interface MSEPlayer extends Player {
         mediaInfo: MSEPlayerMediaInfo;
         statisticsInfo: MSEPlayerStatisticsInfo;
+        switchPrimaryAudio(): void;
+        switchSecondaryAudio(): void;
     }
 
     interface NativePlayer extends Player {
