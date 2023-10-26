4ac25484cf1fc68dac9ced84246df2d167301115
xqq <xqq@xqq.im>
2023-10-27 00:45:15 +0900

    demux/av1-parser: Specify return type of getChromaFormatString() to string

--- d.ts/src/demux/av1-parser.d.ts
+++ d.ts/src/demux/av1-parser.d.ts
@@ -32,6 +32,6 @@ declare class AV1OBUParser {
     };
     static getLevelString(level: number, tier: number): string;
     static getChromaFormat(mono_chrome: boolean, subsampling_x: number, subsampling_y: number): number;
-    static getChromaFormatString(mono_chrome: boolean, subsampling_x: number, subsampling_y: number): "Unknown" | "4:2:0" | "4:2:2" | "4:4:4" | "4:0:0";
+    static getChromaFormatString(mono_chrome: boolean, subsampling_x: number, subsampling_y: number): string;
 }
 export default AV1OBUParser;
